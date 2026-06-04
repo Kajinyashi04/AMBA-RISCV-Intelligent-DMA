10cce7a9b80bc459e49102be5874bbcbf05e87f0
nguyenminhhung <nguyenminhhung221104@gmail.com>
2026-06-05 03:36:03 +0700

    Add makefile, fix comment

--- rtl/core/control_unit.v
+++ rtl/core/control_unit.v
@@ -1,27 +1,23 @@
 module control_unit (
-    // --- INPUT (Từ khối Decoder truyền sang) ---
+
     input  wire[6:0] opcode,
     input  wire [2:0] funct3,
-    input  wire       funct7_5,   // Bit thứ 30 của lệnh (Dùng để phân biệt ADD và SUB)
+    input  wire       funct7_5,  
 
-    // --- OUTPUT (Các công tắc điều khiển hệ thống) ---
-    output reg        reg_write,  // 1 = Ghi vào thanh ghi
-    output reg        alu_src,    // 0 = ALU đọc rs2, 1 = ALU đọc Immediate
-    output reg        mem_write,  // 1 = Ghi ra RAM/Bus
-    output reg        mem_read,   // 1 = Đọc từ RAM/Bus
-    output reg  [1:0] result_src, // MUX chọn kết quả: 00=ALU, 01=RAM, 10=PC+4 (Cho lệnh Nhảy)
-    output reg        branch,     // 1 = Lệnh rẽ nhánh (BEQ, BNE...)
-    output reg        jump,       // 1 = Lệnh nhảy vô điều kiện (JAL, JALR)
-    output reg  [3:0] alu_ctrl    // Lệnh cho ALU (0000=ADD, 1000=SUB...)
+    output reg        reg_write,  
+    output reg        alu_src,    
+    output reg        mem_write,  
+    output reg        mem_read,   
+    output reg  [1:0] result_src, 
+    output reg        branch,     
+    output reg        jump,      
+    output reg  [3:0] alu_ctrl    
 );
 
-    reg [1:0] alu_op; // Tín hiệu trung gian
+    reg [1:0] alu_op; 
 
-    // =========================================================
-    // 1. MAIN DECODER (Bộ giải mã chính - Nhìn vào Opcode)
-    // =========================================================
+
     always @(*) begin
-        // Mặc định tắt hết công tắc cho an toàn
         reg_write = 0; alu_src = 0; mem_write = 0; mem_read = 0; 
         result_src = 2'b00; branch = 0; jump = 0; alu_op = 2'b00;
 
@@ -50,9 +46,7 @@ module control_unit (
         endcase
     end
 
-// =========================================================
-    // 2. ALU DECODER (Hỗ trợ full RV32I)
-    // =========================================================
+
     always @(*) begin
         case (alu_op)
             2'b00: alu_ctrl = 4'b0000; // Load/Store -> CỘNG
